e820ecace2741f8c01cbe5bd60340d787c2dde8f
IrynaKniazieva <gleb0812@gmail.com>
2022-09-08 23:18:34 +0300

    Task -1 work

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,6 @@
 
 // нахожу слушателя:
-const body = document.querySelector('body');
+const bodyEl = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
@@ -17,34 +17,19 @@ function onStartBtnClick() {
     stopBtn.disabled = false;
 
     timerId = setInterval(() => {
-     color = getRandomHexColor;
-     body.style.background = color;  
+     color = getRandomHexColor();
+     bodyEl.style.background = color;  
     }, 1000);
 }
 
 // функция стоп цвет
 function onStopColorClick() {
-
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(timerId);
 }
 
 //  функция рандомный цвет
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
-
-
-//   const startBtn = document.querySelector(".js-start");
-// const stopBtn = document.querySelector(".js-stop");
-// let timerId = null;
-
-// startBtn.addEventListener("click", () => {
-//   timerId = setInterval(() => {
-//     console.log(`I love async JS!  ${Math.random()}`);
-//   }, 1000);
-// });
-
-
-// stopBtn.addEventListener("click", () => {
-//   clearInterval(timerId);
-//   console.log(`Interval with id ${timerId} has stopped!`);
-// });

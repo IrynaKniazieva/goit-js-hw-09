0a72a548a8d9ad3f9be9560d2c1e81b76ea41044
IrynaKniazieva <gleb0812@gmail.com>
2022-09-08 23:05:33 +0300

    Task - 1  в процессе

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,33 @@
 
-
+// нахожу слушателя:
+const body = document.querySelector('body');
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
+
+// слушатели:
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopColorClick);
+
+// глобальная переменная:
 let timerId = null;
 
-startBtn.addEventListener('click' );
-
+// функция старт цвет:
 function onStartBtnClick() {
-    timerId = setInterval (() => {
-        
-    })
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerId = setInterval(() => {
+     color = getRandomHexColor;
+     body.style.background = color;  
+    }, 1000);
 }
 
+// функция стоп цвет
+function onStopColorClick() {
+
+}
+
+//  функция рандомный цвет
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }

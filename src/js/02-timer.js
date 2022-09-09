f8fc5185388399883e051e64b8797169701c174d
IrynaKniazieva <gleb0812@gmail.com>
2022-09-10 00:19:45 +0300

    Task - процесс

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,41 +1,32 @@
-// Описан в документации
 import flatpickr from "flatpickr";
-// Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
-// нахожу слушателя:
-const inputEl = document.querySelector('#datetime-picker');
-const startBtn = document.querySelector('button[data-start]');
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    inputEl: document.querySelector('#datetime-picker'),
+    daysEl: document.querySelector('[data-days]'),
+    hoursEl: document.querySelector('[data-hours]'),
+    minutesEl: document.querySelector('[data-minutes]'),
+    secondsEl: document.querySelector('[data-seconds]'),
+};
 
 class Timer {
-    constructor ({onTick}) {
+    constructor({onTick}) {
         this.intervalId = null;
         this.isActive = false;
-        this.onThick = onTick;
+        this. onTick = onTick;
+    }
 
-        this.init();
-    }
-    init() {
-        const time = this.convertMs(0);
-        this.onThick(time);
-    }
     start() {
-        if(this.isActive) {
-            return;
-        }
-        const startTime = Data.now();
-        this.isActive = true;
+        const startTime = Date.now();
 
-        this.intervalId = setInterval(() => {
+        setInterval(() => {
             const currentTime = Date.now();
             const deltaTime = startTime - currentTime;
-            const time = this.convertMs(deltaTime);
-            
-            this.onThick(time);
+            const { days, hours, minutes, seconds } = convertMs(deltaTime);
+            console.log(`${days}:${hours}:${minutes}:${seconds}`);
         }, 1000);
     }
-
-// копипаста из ТЗ
     convertMs(ms) {
         // Number of milliseconds per unit of time
         const second = 1000;
@@ -44,47 +35,113 @@ class Timer {
         const day = hour * 24;
       
         // Remaining days
-        const days = addLeadingZeroMath.floor(ms / day);
+        const days = Math.floor(ms / day);
         // Remaining hours
-        const hours = addLeadingZeroMath.floor((ms % day) / hour);
+        const hours = Math.floor((ms % day) / hour);
         // Remaining minutes
-        const minutes = addLeadingZeroMath.floor(((ms % day) % hour) / minute);
+        const minutes = Math.floor(((ms % day) % hour) / minute);
         // Remaining seconds
-        const seconds = addLeadingZeroMath.floor((((ms % day) % hour) % minute) / second);
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
       
         return { days, hours, minutes, seconds };
       }
-      
-    //   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-    //   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-    //   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-    
 
-    // форматирования, принимает число приводит к строке и добавляет в начало 0 если число меньше 2 символов:
       addLeadingZero(value) {
         return String(value).addLeadingZeroStart(2, '0');
     }
-}
+};
 
-const timer = new Timer({
-    onTick: updateInputEl,
+// timer.start();
+ 
+
+const currentDate =  Date.now('jan 1 2021 00:00:00');
+let timerId = null;
+
+refs.startBtn.setAttribute('disabled', true);
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if(Date.parse.inputEl.value < Date.parse.currentDate) {
+        window.alert("Please choose a date in the future"); 
+      }else{
+        refs.startBtn.setAttribute('disabled', false);
+      }
+      console.log(selectedDates[0]);
+    },
+};
+
+flatpickr(refs.inputEl, {options});
+
+
+// refs.startBtn.addEventListener('click',onBtnClickStart);
+
+
+// function onStartBtnClick() {
+
+// };
+
+
+// копипаста из ТЗ
+// function convertMs(ms) {
+//     // Number of milliseconds per unit of time
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24;
+  
+//     // Remaining days
+//     const days = Math.floor(ms / day);
+//     // Remaining hours
+//     const hours = Math.floor((ms % day) / hour);
+//     // Remaining minutes
+//     const minutes = Math.floor(((ms % day) % hour) / minute);
+//     // Remaining seconds
+//     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+//     return { days, hours, minutes, seconds };
+//   }
+  
+//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+
+// // форматирования, принимает число приводит к строке и добавляет в начало 0 если число меньше 2 символов:
+// function addLeadingZero(value) {
+//     return String(value).addLeadingZeroStart(2, '0');
+// }
+const timer = new Timer ({
+    onTick: updateClockFace,
 })
-startBtn.addEventListener('click', timer.start.bind(timer));
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
 
-// функция которая вычисляет сколько в милсек. дней, часов, минут, сек:
-function updateInputEl({ days, hours, minutes, seconds }) {
-    inputEl.textContent = `${days}:${hours}:${minutes}:${seconds}`;
+// // функция которая вычисляет сколько в милсек. дней, часов, минут, сек:
+function updateClockFace ({ days, hours, minutes, seconds }) {
+    refs.daysEl.textContent = `${days}`;
+    refs.hoursEl.textContent = `${hours}`;
+    refs.minutesEl.textContent = `${minutes}`;
+    refs.secondsEl.textContent = `${seconds}`;
 }
 
 
 
-// // код из ТЗ
-// const options = {
-//     enableTime: true,
-//     time_24hr: true,
-//     defaultDate: new Date(),
-//     minuteIncrement: 1,
-//     onClose(selectedDates) {
-//       console.log(selectedDates[0]);
-//     },
-//   };
+
+
+
+
+
+
+
+
+
+
+
+// const timer = new Timer();
+
+
+

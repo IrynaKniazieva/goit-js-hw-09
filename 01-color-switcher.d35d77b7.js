1b0e87f250117006afe66f73dc2f7ee37941044f
IrynaKniazieva <103584399+IrynaKniazieva@users.noreply.github.com>
2022-09-13 17:37:57 +0000

    Deploying to gh-pages from @ IrynaKniazieva/goit-js-hw-09@40772b39f2f87df0e505eba789d9b581055f533a 🚀

--- 01-color-switcher.d35d77b7.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,d.disabled=!1,o=setInterval((()=>{color=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.style.background=color}),1e3)})),d.addEventListener("click",(function(){t.disabled=!1,d.disabled=!0,clearInterval(o)}));let o=null;
-//# sourceMappingURL=01-color-switcher.d35d77b7.js.map
